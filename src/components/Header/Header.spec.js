d9e9c8abee121edc7dfe8ce501ce1ab074efec5b
Karen Gamis <kjgamis@live.ca>
2020-03-21 16:25:07 -0400

    data-test refactor

--- src/components/Header/Header.spec.js
+++ src/components/Header/Header.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { findByTestAttr } from './../../utils'
 import Header from '../Header'
 
 const setUp = (props = {}) => {
@@ -14,12 +15,12 @@ describe('Header Component', () => {
   })
 
   it('Should render without errors', () => {
-    const wrapper = component.find(`[data-test='headerComponent']`)
+    const wrapper = findByTestAttr(component, 'headerComponent')
     expect(wrapper.length).toBe(1)
   })
 
   it('Should render a logo', () => {
-    const wrapper = component.find(`[data-test='logoImg']`)
+    const wrapper = findByTestAttr(component, 'logoImg')
     expect(wrapper.length).toBe(1)
   })
 })
