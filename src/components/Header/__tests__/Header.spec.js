a433666ceae6dc09518209f43d1fc0ee2b7ac1f3
Karen Gamis <36368604+kjgamis@users.noreply.github.com>
2020-03-23 11:53:45 -0400

    Testing (#3)
    * start writing tests
    
    * cleanup & refactor
    
    * change className to data-test
    
    * data-test refactor
    
    * create tests for Headline Component

---
+++ src/components/Header/__tests__/Header.spec.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr } from '../../../utils'
+import Header from '..'
+
+const setUp = (props = {}) => {
+  const component = shallow(<Header {...props} />)
+  return component
+}
+
+describe('Header Component', () => {
+  let component
+  beforeEach(() => {
+    component = setUp()
+  })
+
+  it('Should render without errors', () => {
+    const wrapper = findByTestAttr(component, 'headerComponent')
+    expect(wrapper.length).toBe(1)
+  })
+
+  it('Should render a logo', () => {
+    const wrapper = findByTestAttr(component, 'logoImg')
+    expect(wrapper.length).toBe(1)
+  })
+})
